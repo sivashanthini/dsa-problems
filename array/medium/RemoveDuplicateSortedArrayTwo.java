bfbf758ac63ac45878f8e0662a5d0fce2e9f2a0e
sishanth <siva.shanthini.e.g>
2023-08-22 11:12:53 +0530

    Add questions

--- array/medium/RemoveDuplicateSortedArrayTwo.java
+++ array/medium/RemoveDuplicateSortedArrayTwo.java
@@ -32,7 +32,24 @@ package array.medium;
 import java.util.HashMap;
 
 public class RemoveDuplicateSortedArrayTwo {
+
     public int removeDuplicates(int[] nums) {
+        if (nums.length <= 2) {
+            return nums.length;
+        }
+
+        int index = 2;
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[index - 2]) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+    public int removeDuplicatesApproachOne(int[] nums) {
 
         int j = 1, count = 1, num = nums[0];
 
