2625c43c483fb518e2908fa6e644d669244fdfda
sishanth <siva.shanthini.e.g>
2023-09-13 01:05:03 +0530

    Add questions

--- array/hard/BestTimeToBuyAndSellStockThree.java
+++ array/hard/BestTimeToBuyAndSellStockThree.java
@@ -64,4 +64,25 @@ public class BestTimeToBuyAndSellStockThree {
         return sell2;
 
     }
+
+    //In the forward pass, it is calculating the maximum profit at day i
+    //In the reverse pass, it calculates the second maximum at day i and then finds the total profit by adding the profit from the forward pass at day i-1.
+    public int maxProfitTwo(int[] prices) {
+        int n=prices.length, pass[] = new int[n], buy=Integer.MAX_VALUE, sell=0, prof=0, i;
+        // Pass 1: Forward
+        for(i=0; i<n; i++) {
+            if(buy>prices[i]) buy=sell=prices[i];
+            sell=Math.max(sell, prices[i]);
+            pass[i]=prof=Math.max(prof, sell-buy);
+        }
+
+        // Pass 2: Reversed
+        int res=pass[n-1];
+        for(i=n-1, sell=prof=0, buy=Integer.MAX_VALUE; i>0; --i) {
+            if(sell<prices[i]) sell=buy=prices[i];
+            buy=Math.min(buy, prices[i]);
+            res=Math.max(res, pass[i-1]+sell-buy);
+        }
+        return res;
+    }
 }
