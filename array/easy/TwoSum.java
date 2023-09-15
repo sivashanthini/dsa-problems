e2f5f30989e091928f55a4cce147378be428f5f4
sishanth <siva.shanthini.e.g>
2023-09-15 16:07:13 +0530

    Add questions

--- array/easy/TwoSum.java
+++ array/easy/TwoSum.java
@@ -29,20 +29,19 @@ import java.util.HashMap;
 
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> numMap = new HashMap();
-        int diff = 0, i = 0;
-        int[] sumIndices = new int[2];
-        for (int number : nums) {
-            diff = target - number;
-            if (numMap.containsKey(diff)) {
-                sumIndices[0] = numMap.get(diff);
-                sumIndices[1] = i;
-                return sumIndices;
-            } else {
-                numMap.put(number, i);
+        int[] result = new int[2];
+        if (nums == null || nums.length <= 1)
+            return result;
+        HashMap<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (!map.containsKey(diff)) map.put(nums[i], i);
+            else {
+                result[0] = (int) map.get(diff);
+                result[1] = i;
+                break;
             }
-            i++;  
         }
-        return sumIndices;
+        return result;
     }
 }
