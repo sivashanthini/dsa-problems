09ea4096d050cb5fc46c5a34e42900c5838d3f5f
sishanth <siva.shanthini.e.g>
2024-01-09 11:04:30 +0530

    Add questions

--- tree/medium/DeserializeBinaryTree.java
+++ tree/medium/DeserializeBinaryTree.java
@@ -4,42 +4,28 @@ You are given an integer array A denoting the Level Order Traversal of the Binar
 You have to Deserialize the given Traversal in the Binary Tree and return the root of the Binary Tree.
 
 NOTE:
-
     In the array, the NULL/None child is denoted by -1.
     For more clarification check the Example Input.
 
-
-
 Problem Constraints
-
 1 <= number of nodes <= 105
-
 -1 <= A[i] <= 105
 
-
-
 Input Format
-
 Only argument is an integer array A denoting the Level Order Traversal of the Binary Tree.
 
 Output Format
-
 Return the root node of the Binary Tree.
 
 Example Input
-
 Input 1:
-
  A = [1, 2, 3, 4, 5, -1, -1, -1, -1, -1, -1]
 
 Input 2:
-
  A = [1, 2, 3, 4, 5, -1, 6, -1, -1, -1, -1, -1, -1]
 
 Example Output
-
 Output 1:
-
            1
          /   \
         2     3
@@ -47,7 +33,6 @@ Output 1:
       4   5
 
 Output 2:
-
             1
           /   \
          2     3
