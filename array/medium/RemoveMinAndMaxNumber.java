6782c8e6987dfeddd44b31294b518e9af024a04c
sishanth <siva.shanthini.e.g>
2024-04-19 22:53:09 +0530

    Add questions

--- array/medium/RemoveMinAndMaxNumber.java
+++ array/medium/RemoveMinAndMaxNumber.java
@@ -1,13 +1,8 @@
 /*
-ou are given a 0-indexed array of distinct integers nums.
-
+You are given a 0-indexed array of distinct integers nums.
 There is an element in nums that has the lowest value and an element that has the highest value. We call them the minimum and maximum respectively. Your goal is to remove both these elements from the array.
-
 A deletion is defined as either removing an element from the front of the array or removing an element from the back of the array.
-
 Return the minimum number of deletions it would take to remove both the minimum and maximum element from the array.
-
-
 
 Example 1:
 
@@ -38,7 +33,6 @@ There is only one element in the array, which makes it both the minimum and maxi
 We can remove it with 1 deletion.
 
 Constraints:
-
     1 <= nums.length <= 105
     -105 <= nums[i] <= 105
     The integers in nums are distinct.
